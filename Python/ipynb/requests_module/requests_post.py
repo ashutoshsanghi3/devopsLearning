128597cbb002bbf0c2ea4dad59e4545bb1ee99c9
ashutoshsanghi3 <ashutoshsanghi3@gmail.com>
2025-02-25 11:10:54 +0530

    fixing errors in file managing

--- Python/ipynb/requests_module/requests_post.py
+++
@@ -1,4 +0,0 @@
-import requests
-response=requests.post("https://httpbin.org/post",data={'key':'value'})
-print(response.status_code)
-print(response.content)
--- Python/ipynb/requests_module/requests_put.py
+++
@@ -1,4 +0,0 @@
-import requests
-response=requests.put("https://httpbin.org/put",data={'key':'value'})
-print(response.status_code)
-print(response.content)
